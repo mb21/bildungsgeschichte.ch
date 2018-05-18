caf6bcbaedc35f62e012d7bb43ebe93f5882115b
mb21 <mb21@users.noreply.github.com>
2018-05-18 14:15:24 +0200

    Header: remove unused imports

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,13 @@
 import React    from 'react'
 import { Link } from 'react-router-dom'
 
-import {translate, getBaseUrl, langs, getCurrentLang, getSwitchToLangUrl} from '../../utils/translate'
+import {translate, getBaseUrl} from '../../utils/translate'
 
 import './Header.css'
 import Logo from './logo-wbgs.svg'
 
 class Header extends React.Component {
   render() {
-    const currentLang = getCurrentLang();
     return (
       <header className="Header">
         <Link to={ getBaseUrl() }>
