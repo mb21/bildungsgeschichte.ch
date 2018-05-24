72739c6ae4531f94a91c9b0d5dba12c74a67e29b
mb21 <mb21@users.noreply.github.com>
2018-05-24 10:06:16 +0200

    fix footer links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,18 @@
 import React        from 'react'
 import { Link }     from 'react-router-dom'
 
-import {translate}  from '../../utils/translate'
+import {translate, getCurrentLang}  from '../../utils/translate'
 
 import './Footer.css'
 
 class Footer extends React.Component {
   render() {
+    const prefix = '/' + getCurrentLang() + '/';
     return (
       <footer className="Footer">
-        <Link className="link" to="about">{ this.props.strings.about }</Link>
-        <Link className="link" to="projects">{ this.props.strings.projects }</Link>
-        <Link className="link" to="rights">{ this.props.strings.rights }</Link>
+        <Link className="link" to={ prefix + 'about'}>{ this.props.strings.about }</Link>
+        <Link className="link" to={ prefix + 'projects'}>{ this.props.strings.projects }</Link>
+        <Link className="link" to={ prefix + 'rights'}>{ this.props.strings.rights }</Link>
         <p className="copyright">{ this.props.strings.copyright }</p>
       </footer>
     )
