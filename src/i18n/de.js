8668c71190c3c455063b9fc838853fb2ff6457af
Andreas Baumann <mail@andreasbaumann.cc>
2019-03-25 17:18:29 +0100

    fixed a typo

--- src/i18n/de.js
+++ src/i18n/de.js
@@ -46,7 +46,7 @@ export default {
 <h4 id="projektkernteam-technische-entwicklungsphase">Projektkernteam (technische Entwicklungsphase)</h4>
 <p>Christina Rothen; Thomas Ruoss; Lucien Criblez (Universität Zürich)<br />
 Projektleitung
-<p>Peter Schäuble (<a href="https://www.eurospider.com/de/">Eurospider Information Technology AB)</a><br />
+<p>Peter Schäuble (<a href="https://www.eurospider.com/de/">Eurospider Information Technology AG)</a><br />
 Entwicklung der Suchmaschine</p>
 <p>Amanda Sauter &amp; Mauro Bieg (2324.ch)<br />
 Design und Entwicklung des Frontends</p>
